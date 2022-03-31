bfc17e74a0363d095cf794c681a7e7d46f376701
SidddhantDeshpande <87174561+SidddhantDeshpande@users.noreply.github.com>
2022-03-31 17:48:37 +0530

    Initial 2

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      colors:{
+        "time-purple":"#1d1b67",
+        "hov-time-purple":"#2926A7"
+        
+
+      },
+
+      fontFamily:{
+      Inter: ["Inter,sans-serif"],
+      
+    },
+
+  
+
+    },
+    container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg: "1124px",
+        xl: "1124px",
+        "2xl" : "1124px"
+      }
+    }
+  },
+  plugins: [],
+}
